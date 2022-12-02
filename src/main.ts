8622f76768e22074b67858ce08e0e24dc91c145c
Gaspar Neto <gaspar.neto@voxy.com>
2022-12-02 05:57:12 -0300

    Handle case when characters are not found and return a resonable message

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,28 @@
 import {getCharacters, Character} from './getCharacters'
 
 export async function battle(heroName: string, villainName: string): Promise<Character> {
-  const characters = await getCharacters()
+  const characters = await getCharacters();
+  const hero = characters.items.find(e => e.name === heroName);
+  const villain = characters.items.find(e => e.name === villainName);
+  assert_characters_exist(hero!, heroName, villain!, villainName);
 
-  const hero = characters.items.find(e => e.name === heroName)
-  const villain = characters.items.find(e => e.name === villainName)
-  if(!hero || ! villain) {
-    throw new Error("Character not found. Try again with another name.")
-  }
-  return gets_winner(hero!, villain!);;
+  return gets_winner(hero!, villain!);
+}
+
+function assert_characters_exist(hero: Character, heroName: string, villain: Character, villainName: string) {
+  let message = ''
+  if(!hero)
+    message = `Character ${heroName} not found. Try again with another name.`
+  if(!villain)
+    message = `Character ${villainName} not found. Try again with another name.`
+  if(!villain && !hero)
+    message = `Characters ${heroName} and ${villainName} not found. Try again with another names.`
+  if (message)
+    throw new Error(message)
 }
 
 function gets_winner(hero: Character, villain: Character): Character {
-  return hero!.score >= villain!.score ? hero! : villain!
+  if (hero!.score >= villain!.score)
+    return hero
+  return villain
 }
--- src/mainIntegration.spec.ts
+++ src/mainIntegration.spec.ts
@@ -1,4 +1,5 @@
 import {battle} from "./main";
+import {Character} from "./getCharacters";
 
 it('battle should return the hero when API is called', async () => {
   const result = await battle("Batman", 'Joker');
@@ -6,6 +7,14 @@ it('battle should return the hero when API is called', async () => {
   expect(result).toEqual({name: "Batman", score: 8.3, type: "hero", weakness: "Joker"});
 })
 
-it('battle should throw error when character not found', async () => {
-  await expect(battle("Random", "Joker")).rejects.toThrow("Character not found. Try again with another name.");
+it('battle should throw error when hero character not found', async () => {
+  await expect(battle("Random", "Joker")).rejects.toThrow("Character Random not found. Try again with another name.");
+})
+
+it('battle should throw error when villain character not found', async () => {
+  await expect(battle("Batman", "Random")).rejects.toThrow("Character Random not found. Try again with another name.");
+})
+
+it('battle should throw error when both characters character not found', async () => {
+  await expect(battle("Random", "Random")).rejects.toThrow("Characters Random and Random not found. Try again with another names.");
 })
